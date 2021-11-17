b587a70c4f504495d803e12a16c826020d380f6d
Lewis Njagi <lewismaybach@gmail.com>
2021-11-17 16:49:33 +0300

    add maz external library

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { MazPhoneNumberInput } from "maz-ui";
 
 Vue.config.productionTip = false;
+Vue.use(MazPhoneNumberInput);
 
 new Vue({
   router,
