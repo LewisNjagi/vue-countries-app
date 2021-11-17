e594ae27662838953dbebd02016f329020109e1f
Lewis Njagi <lewismaybach@gmail.com>
2021-11-17 16:51:13 +0300

    display phone validation input

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,1 +1,17 @@
+<template>
+  <div id="app">
+    <MazPhoneNumberInput v-model="phoneNumber" />
+  </div>
+</template>
 
+<script>
+import { MazPhoneNumberInput } from "maz-ui";
+export default {
+  components: { MazPhoneNumberInput },
+  data() {
+    return {
+      phoneNumber: null,
+    };
+  },
+};
+</script>
