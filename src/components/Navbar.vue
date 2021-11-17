217ec47e8b2f6ce2e1f1956762aa00bf9517d60f
Lewis Njagi <lewismaybach@gmail.com>
2021-11-17 12:03:53 +0300

    add axios

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">Countries</b-navbar-brand>
+      <b-navbar-brand href="/">Countries</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Home</b-nav-item>
-          <b-nav-item href="#" disabled>Country Code</b-nav-item>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/about">Country Code</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
@@ -30,4 +30,8 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+#nav a.router-link-exact-active {
+  color: white;
+}
+</style>
